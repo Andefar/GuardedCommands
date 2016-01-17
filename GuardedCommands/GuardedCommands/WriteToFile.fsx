b24f08af3e411529ee340d040c86efee1c545447
SilasPihl <pihl92@hotmail.com>
2016-01-17 14:38:51 +0100

    sd

---
+++ GuardedCommands/GuardedCommands/WriteToFile.fsx
@@ -0,0 +1,23 @@
+﻿let localPath = @"C:\Users\Silas\Dropbox\5. Semester\02257 - Anvendt funktionsprogrammering\Project 2\GuardedCommands\GuardedCommands\GuardedCommands\"
+#r @".\bin\Debug\FSharp.PowerPack.dll";
+#r @".\bin\Debug\Machine.dll";
+#r @".\bin\Debug\\VirtualMachine.dll";
+
+#load "AST.fs"
+#load "Parser.fs"
+#load "Lexer.fs"
+#load "TypeCheck.fs"
+#load "CodeGen.fs"
+#load "CodeGenOpt.fs"
+#load "Util.fs"
+
+open GuardedCommands.Util
+open GuardedCommands.Frontend.TypeCheck
+open GuardedCommands.Frontend.AST
+open GuardedCommands.Backend.CodeGeneration
+open ParserUtil
+open CompilerUtil
+open Machine
+open VirtualMachine
+
+System.IO.Directory.SetCurrentDirectory localPath;
